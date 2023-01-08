bebc34cd230508494df21904fe8d461e0afd7eac
KurrXd <91126186+KurrXd@users.noreply.github.com>
2023-01-09 00:05:01 +0700

    Update tess.sh

--- tess.sh
+++ tess.sh
@@ -109,7 +109,7 @@ while [ "$done" == false ]; do
     output "[$i] ${options[$i]}"
   done
 
-  echo -n "input pilihan : "
+  echo -n "*input pilihan : "
   read -r action
 
   [ -z "$action" ] && error "Input is required" && continue
